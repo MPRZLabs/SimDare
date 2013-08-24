e8228de58eff184be28c4154f745c2da4cde73ef
Michcioperz <michcioperz@gmail.com>
2013-08-24 20:13:39 +0000

    rendering bed now, as well as protagonist depending on where it is.

--- main.lua
+++ main.lua
@@ -14,6 +14,7 @@ function love.load()
     [12] = love.graphics.newFont(12),
     [20] = love.graphics.newFont(20)
   }
+  occupation = 3
   love.graphics.setFont(fonts[12])
 end
 
@@ -26,10 +27,16 @@ function love.draw()
   arc.clear_key()
   love.graphics.draw(sprites[3], 0, 0)
   love.graphics.draw(sprites[2], 571, 238, 0, 4, 4, 8, 12)
-  love.graphics.draw(sprites[4], 220, 300, -70, 5)
+  love.graphics.draw(sprites[4], 220, 300, math.rad(-70), 5)
+  if occupation == 3 then
+    love.graphics.draw(sprites[1], 270, 200, math.rad(20), 4)
+  elseif occupation == 2 then
+    love.graphics.draw(sprites[1], -10, -10, 0, 4, 4)
+  elseif occupation == 1 then
+    love.graphics.draw(sprites[1], 543, 226, 0, 4, 4)
+  end
 end
 
 function love.keypressed(k, unicode)
   arc.set_key(k)
 end
-
