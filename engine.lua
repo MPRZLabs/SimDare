0f4e42c717305cec99c4c3cbbf9443c5039bb2be
Michał Sidor <michcioperz@autistici.org>
2013-11-19 20:45:43 +0100

    Made more complications

--- engine.lua
+++ engine.lua
@@ -1,19 +1,20 @@
 Soupalion = {}
-Soupalion.l = {}
-function Soupalion.l:inf(cnme, text)
+Soupalion.logger = {}
+Soupalion.l = Soupalion.logger
+function Soupalion.logger:inf(cnme, text)
 	print("[" .. cnme .. "] " .. text)
 end
-function Soupalion.l:infl(cnme,text)
+function Soupalion.logger:infl(cnme,text)
 	print("[" .. cnme .. "] " .. text)
 	print()
 end
 function Soupalion:init(codename,version,visname,author)
-	Soupalion.l:inf("engine", "Initialisation called")
+	Soupalion.logger:inf("engine", "Initialisation called")
 	Soupalion.codename = codename
 	Soupalion.version = version
 	Soupalion.name = visname
 	Soupalion.author = author
-	Soupalion.l:inf("engine", "Basic init performed")
-	Soupalion.l:inf("engine", "Game starting: "..Soupalion.name.." v"..Soupalion.version.." by "..Soupalion.author)
+	Soupalion.logger:inf("engine", "Basic init performed")
+	Soupalion.logger:inf("engine", "Game starting: "..Soupalion.name.." v"..Soupalion.version.." by "..Soupalion.author)
 end
 return Soupalion
